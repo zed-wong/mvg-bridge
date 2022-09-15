d86cbf82f012af799e2deeb53095abc380ee3438
zedwong <kingflyme1@gmail.com>
2022-09-15 19:32:43 +0900

    Fix search bar css

--- nft/selectFromNetwork.vue
+++ nft/selectFromNetwork.vue
@@ -15,7 +15,7 @@
             outlined
             v-model="search"
             hide-details="true"
-            class="search-border"
+            class="search-border search-bg"
             prepend-inner-icon="mdi-magnify"
             :placeholder="$t('search_network_name')"
           />
